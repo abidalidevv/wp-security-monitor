62f4a2622edc819fa14d589a27fb7a3a147a112b
abidmmp <abidmmp99@gmail.com>
2018-05-14 10:07:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -222,3 +222,4 @@
 // Optimize database queries - 2021-06-26 10:05:00
 // Improve API response parsing - 2024-11-03 10:01:00
 // Add pagination support - 2022-08-22 10:11:00
+// Simplify conditional logic - 2018-05-14 10:07:00
