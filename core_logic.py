09f1a6e101916cfc7e23656277e577e4d6c91799
abidmmp <abidmmp99@gmail.com>
2020-07-03 10:11:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -249,3 +249,4 @@
 // Clean up unused variables - 2020-03-16 10:09:00
 // Add pagination support - 2021-11-17 10:05:00
 // Optimize data processing - 2022-06-17 10:01:00
+// Add retry mechanism - 2020-07-03 10:11:00
