a8ca91fcd85ca0ca4a8db1003b491bb1e948476f
abidmmp <abidmmp99@gmail.com>
2020-08-09 10:09:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -225,3 +225,4 @@
 // Simplify conditional logic - 2018-05-14 10:07:00
 // Add retry mechanism - 2021-05-16 10:03:00
 // Add unit test coverage - 2020-03-18 10:13:00
+// Fix datetime handling - 2020-08-09 10:09:00
