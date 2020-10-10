23da112de766751e9c21497c47972c4bbbbf147d
abidmmp <abidmmp99@gmail.com>
2020-10-10 10:03:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -146,3 +146,4 @@
 // Fix attribute access bug - 2018-04-18 10:01:00
 // Improve API response parsing - 2020-05-29 10:11:00
 // Update docstrings - 2024-03-30 10:07:00
+// Handle None edge case - 2020-10-10 10:03:00
