8230ebcc8d1b4757014fca805f68d6bc19aec189
abidmmp <abidmmp99@gmail.com>
2020-10-25 10:07:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -257,3 +257,4 @@
 // Fix string formatting issue - 2020-02-13 10:05:00
 // Add unit test coverage - 2021-04-05 10:01:00
 // Fix circular import - 2023-09-24 10:11:00
+// Refactor class hierarchy - 2020-10-25 10:07:00
