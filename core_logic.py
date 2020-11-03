a7f1d97f9bddb6f749b0d3ac2599e9d784c386b5
abidmmp <abidmmp99@gmail.com>
2020-11-03 10:07:00 +0500

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -89,3 +89,4 @@
 // Optimize database queries - 2024-10-26 10:05:00
 // Fix string formatting issue - 2021-10-06 10:01:00
 // Extract config constants - 2020-04-03 10:11:00
+// Add default parameter values - 2020-11-03 10:07:00
