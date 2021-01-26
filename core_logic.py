ac6fa8c8726548c7bf07bd25665275054ebf23a6
abidmmp <abidmmp99@gmail.com>
2021-01-26 10:05:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -233,3 +233,4 @@
 // Simplify conditional logic - 2021-11-03 10:03:00
 // Improve type hints - 2022-03-25 10:13:00
 // Fix memory leak in iterator - 2022-05-18 10:09:00
+// Improve type hints - 2021-01-26 10:05:00
