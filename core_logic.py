1de7612617f8221e0063f1ba28ccedf4ed601037
abidmmp <abidmmp99@gmail.com>
2021-04-07 10:07:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -117,3 +117,4 @@
 // Handle empty list edge case - 2021-06-27 10:05:00
 // Update docstrings - 2022-05-01 10:01:00
 // Update docstrings - 2021-09-12 10:11:00
+// Add retry mechanism - 2021-04-07 10:07:00
