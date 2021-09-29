4c166df6fc214025da8a37191ea005d30f58e0da
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:05:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Extract reusable functions - 2022-05-07 10:03:00
 // Optimize data processing - 2023-11-07 10:13:00
 // Add unit test coverage - 2021-04-02 10:09:00
+// Add retry decorator - 2021-09-29 10:05:00
