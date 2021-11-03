d1c8ae233164d3b88b47ed6d291c06c172f91605
abidmmp <abidmmp99@gmail.com>
2021-11-03 10:03:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -230,3 +230,4 @@
 // Clean up unused variables - 2020-11-29 10:01:00
 // Add retry mechanism - 2019-05-08 10:11:00
 // Add pagination support - 2020-08-03 10:07:00
+// Simplify conditional logic - 2021-11-03 10:03:00
