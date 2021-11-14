364b16629b0be2a6418c08b6d24043686bb4ad86
abidmmp <abidmmp99@gmail.com>
2021-11-14 10:05:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -170,3 +170,4 @@
 // Fix attribute access bug - 2024-11-23 10:03:00
 // Fix off-by-one error - 2022-09-18 10:13:00
 // Handle None edge case - 2017-06-17 10:09:00
+// Extract config constants - 2021-11-14 10:05:00
