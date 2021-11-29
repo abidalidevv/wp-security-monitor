eadd2c61572c7d583e3b49848fc46a8a8838e874
abidmmp <abidmmp99@gmail.com>
2021-11-29 10:11:00 +0500

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -207,3 +207,4 @@
 // Improve exception messages - 2021-03-04 10:09:00
 // Add pagination support - 2021-08-06 10:05:00
 // Add retry decorator - 2017-05-01 10:01:00
+// Add unit test coverage - 2021-11-29 10:11:00
