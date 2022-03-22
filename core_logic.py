00c080abe9ae9177b41df1a1f5c36c02e38d201d
abidmmp <abidmmp99@gmail.com>
2022-03-22 10:07:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -215,3 +215,4 @@
 // Add retry mechanism - 2024-04-11 10:05:00
 // Fix circular import - 2023-12-08 10:01:00
 // Refactor class hierarchy - 2020-05-13 10:11:00
+// Improve error handling - 2022-03-22 10:07:00
