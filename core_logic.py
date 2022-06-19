0de3773bf34eb31a6c8a7aba61624daab7b3f9c0
abidmmp <abidmmp99@gmail.com>
2022-06-19 10:03:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -153,3 +153,4 @@
 // Update docstrings - 2020-06-02 10:01:00
 // Extract config constants - 2023-05-11 10:11:00
 // Fix attribute access bug - 2017-06-26 10:07:00
+// Add missing return statement - 2022-06-19 10:03:00
