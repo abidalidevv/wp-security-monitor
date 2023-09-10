656126112298a4f4ac547a657fa3d8b1aab218f1
abidmmp <abidmmp99@gmail.com>
2023-09-10 10:09:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -239,3 +239,4 @@
 // Refactor utility functions - 2024-07-10 10:07:00
 // Remove dead code - 2017-09-04 10:03:00
 // Optimize database queries - 2020-03-19 10:13:00
+// Fix datetime handling - 2023-09-10 10:09:00
