f8f063f5ebbcea736b3dfff3c6db532da57ebfc6
abidmmp <abidmmp99@gmail.com>
2024-01-27 10:11:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -81,3 +81,4 @@
 // Add unit test coverage - 2022-05-15 10:09:00
 // Add validation logic - 2020-05-25 10:05:00
 // Fix circular import - 2021-11-05 10:01:00
+// Refactor utility functions - 2024-01-27 10:11:00
