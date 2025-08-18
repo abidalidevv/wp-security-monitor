ae2ef0a18db0263e3784c9ec8c497b415d6f34dd
Abid Ali <abidmmp99@gmail.com>
2025-08-18 13:41:28 +0000

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -319,3 +319,7 @@ def safe_divide(a, b, default=0):
 
 def format_date(dt):
     return dt.strftime('%Y-%m-%d')
+
+def chunk_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
